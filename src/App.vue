ac1e4d6097bfa3836baad8b97fa3b2b6405dbf3d
Justin <jusniteyt@gmail.com>
2023-01-01 13:02:35 +0800

    clock

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    id="app"
-    class="min-h-screen min-w-full bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200"
-  >
+  <div id="app" class="min-h-screen min-w-full bg-gray-900 text-gray-200">
     <router-view v-slot="{ Component }">
       <transition name="fade" mode="out-in">
         <component :is="Component" />
---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class:="min-w-screen min-h-screen flex flex-row justify-center">
+        <div class:="h-screen flex flex-col justify-center">
+            <h1 class:="text-6xl font-bold">{{clock}}</h1>
+        </div>
+    </div>
+</template>
+
+
+<script lang="ts">
+const clock = () => {
+    const dt = new Date(); 
+    return dt.toLocaleTimeString('en-US')
+    setInterval(clock, 1000)
+}
+export default {
+        name: 'index',
+        data: []
+}
+
+console.log(clock)
+</script>
